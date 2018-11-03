18ccb435f746578331ecd9d7eadf59c2a7487682
Mikhail Proniushkin <mikhailpro4@gmail.com>
2018-11-03 17:09:37 -0400

    react-pose setup

--- src/client/react/pages/home/index.js
+++ src/client/react/pages/home/index.js
@@ -2,8 +2,29 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
+import posed, { PoseGroup } from 'react-pose';
+
+const Of = posed.div({
+  exit: { opacity: 0 },
+  enter: { opacity: 1 }
+});
+
+const OfText = posed.div({
+  exit: { opacity: 0 },
+  enter: {
+		opacity: 1 ,
+		transition: { duration: 1050 }
+	}
+});
+
 
 class HomePage extends Component {
+	state = { isVisible: false };
+
+	componentDidMount() {
+    this.setState({ isVisible: !this.state.isVisible });
+  }
+
 	renderHead = () => (
 		<Helmet>
 			<title>OF – Home Page</title>
@@ -11,10 +32,20 @@ class HomePage extends Component {
 		</Helmet>
 	);
 	render() {
+		const { isVisible } = this.state;
+
 		return (
 				<div className="route-page-container">
-          Home
+					<PoseGroup>
+						{isVisible && [
+		          <Of key="Of" className="of-container" >
+								<OfText key="OfText" className="of-text">New York</OfText>
+							</Of>,
+		        ]}
+					</PoseGroup>
 			</div>
+
+
 		);
 	}
 }
