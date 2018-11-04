43039801b4657d4ed79c5479a1debcfec645a6a9
Mikhail Proniushkin <mikhailpro4@gmail.com>
2018-11-03 22:41:59 -0400

    word animations

--- src/client/react/pages/home/index.js
+++ src/client/react/pages/home/index.js
@@ -3,18 +3,19 @@ import { connect } from "react-redux";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 import posed, { PoseGroup } from 'react-pose';
-import { tween } from 'popmotion'
+import SplitText from 'react-pose-text';
 
-const OfText = posed.div({
+const charPoses = {
   exit: { translateY: 20, opacity: 0 },
   enter: {
     translateY: 0,
     opacity: 1,
     transition: {
-     duration: 400
-    }
+     duration: 400,
+    },
+    delay: ({ wordIndex }) => wordIndex * 150
   }
-});
+};
 
 class HomePage extends Component {
 	state = { isVisible: false };
@@ -39,10 +40,11 @@ class HomePage extends Component {
             <div className="of-grid-logo">
               <div className="of-container">
                 {isVisible && (
-                  <OfText
+                  <SplitText
                     className="of-text"
                     initialPose="exit"
                     pose="enter"
+                    wordPoses={charPoses}
                     onPoseComplete={()=> {console.log("complete")}}
                     onValueChange={{
                       translateY: v => {console.log("translateY: ", v)},
@@ -50,8 +52,7 @@ class HomePage extends Component {
                     }}
                   >
                     Olena Finch
-                  </OfText
-                  >
+                  </SplitText>
                 )}
               </div>
             </div>
