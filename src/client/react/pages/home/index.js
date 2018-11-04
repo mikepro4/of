5a060646ab042285606357cec7d457e12e570edf
Mikhail Proniushkin <mikhailpro4@gmail.com>
2018-11-03 22:27:53 -0400

    animation callbacks

--- src/client/react/pages/home/index.js
+++ src/client/react/pages/home/index.js
@@ -3,20 +3,18 @@ import { connect } from "react-redux";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
 import posed, { PoseGroup } from 'react-pose';
-
-const Of = posed.div({
-  exit: { opacity: 0 },
-  enter: { opacity: 1 }
-});
+import { tween } from 'popmotion'
 
 const OfText = posed.div({
-  exit: { opacity: 0 },
+  exit: { translateY: 20, opacity: 0 },
   enter: {
-		opacity: 1 ,
-		transition: { duration: 1050 }
-	}
+    translateY: 0,
+    opacity: 1,
+    transition: {
+     duration: 400
+    }
+  }
 });
-
 
 class HomePage extends Component {
 	state = { isVisible: false };
@@ -39,7 +37,23 @@ class HomePage extends Component {
           <div className="of-grid">
 
             <div className="of-grid-logo">
-              Logo
+              <div className="of-container">
+                {isVisible && (
+                  <OfText
+                    className="of-text"
+                    initialPose="exit"
+                    pose="enter"
+                    onPoseComplete={()=> {console.log("complete")}}
+                    onValueChange={{
+                      translateY: v => {console.log("translateY: ", v)},
+                      opacity: v => {console.log("opacity: ", v)}
+                    }}
+                  >
+                    Olena Finch
+                  </OfText
+                  >
+                )}
+              </div>
             </div>
 
             <div className="of-grid-images">
@@ -54,13 +68,21 @@ class HomePage extends Component {
               <div className="image-3">
                 image
               </div>
+
+              <div className="image-4">
+                image
+              </div>
+
+              <div className="image-5">
+                image
+              </div>
             </div>
 
             <div className="of-grid-navigation">
               Navigation
             </div>
 
-            <div className="of-grid-13 of-grid-gutter-4 of-grid-content">
+            <div className="of-grid-15 of-grid-gutter-4 of-grid-content">
               content
             </div>
           </div>
